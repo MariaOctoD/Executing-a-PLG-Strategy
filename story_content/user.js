c6bac5191e75d5628b59b141c87d083797df7296
MariaOctoD <maria@octod.ca>
2022-05-12 17:22:28 -0400

    cqswdew

---
+++ story_content/user.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67PhkJvSsxc":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+ var line = document.createElement("audio");
+ var head=document.getElementsByTagName('body')[0];
+ line.type = "audio/mp3";
+ line.src="";
+ line.id="bgSong" ;
+ line.autoplay = true;
+ line.loop = true;
+ head.appendChild(line);
+}
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+ add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+var player = GetPlayer();
+this.Location= player.GetVar("location");
+var audio = document.getElementById('bgSong');
+audio.src= location + "GreenTuesday.mp3";
+audio.load();
+audio.play();
+}
+
